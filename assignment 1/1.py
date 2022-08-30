4c0b1f4413db8d23ef5454ffc456a5b2b770d767
Shubham123ssr <100044183+Shubham123ssr@users.noreply.github.com>
2022-08-30 22:54:08 +0530

    Add files via upload

---
+++ assignment 1/1.py
@@ -0,0 +1,48 @@
+#q4
+'''
+print("Hello Python")
+'''
+#q5
+'''
+print("Hello")
+print("world")
+'''
+#q6
+'''
+print("MySirG")
+'''
+#q7
+'''
+print("Teacher's Day")
+'''
+#q8
+'''
+name="shubham"
+print(name)
+'''
+#q9
+
+'''
+
+name="shubham"
+age=19
+qualification="b.tech"
+yearsofexperiencing=3
+print("My name is "+name)
+print("I am  "+str(age)+" "+"years"+"old")
+print("i am pursuing computer science and engineering as 1st year in "+qualification+"programme in NIT SILCHAR ")
+print ("Till now i have worked for "+str(yearsofexperiencing))
+'''
+#q10
+'''
+a=3
+b=5
+c=6
+print(a)
+print(b)
+print(c)
+'''
+
+
+
+
